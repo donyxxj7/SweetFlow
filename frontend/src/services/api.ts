3f9e130707643a0a92392dbb956a079450838706
Donyzeteee <endonyparadela2007@gmail.com>
2026-06-12 23:10:52 -0300

    feat: alterando url da api diretamente no axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,10 @@
 // frontend/src/services/api.ts
 import axios from "axios";
 
+// Substitua o localhost diretamente pelo link oficial da sua API na Vercel
+// ATENÇÃO: Deixe SEM a barra "/" no final do link!
 export const api = axios.create({
-  // Conecta direto no servidor Fastify que você acabou de ligar
-  baseURL: "http://localhost:3333",
+  baseURL: "https://sweet-flow-nine.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
